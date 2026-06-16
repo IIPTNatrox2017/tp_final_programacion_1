3a2fe69d5cc28fa2b26ba4741d9eb22ad3f88266
Natrox2017 <natroxsolosteam1942@gmail.com>
2026-06-16 18:04:22 -0300

    se implementaron las funciones junto a sus validaciones de los puntajes a los juegos.

--- LIBRERIAS/DOMINIO/entidades/puntajes.c
+++ LIBRERIAS/DOMINIO/entidades/puntajes.c
@@ -1,15 +1,57 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "../cabeceraEntidades/puntajes.h"
 
+#define VAL_MIN 0.0f
+#define VAL_MAX 100.0f
+
 Puntaje crearPuntaje(float valor)
 {
-	printf("todavia no se implemento la funcion\n");
-	Puntaje p = { 0 };
-	return p;
+	Puntaje nuevoPuntaje;
+	if (valor < VAL_MIN || valor > VAL_MAX)
+	{
+		nuevoPuntaje.valor = 0.0;
+		nuevoPuntaje.esValido = 0;
+	}
+	else
+	{
+		nuevoPuntaje.valor = valor;
+		nuevoPuntaje.esValido = 1;
+	}
+	return nuevoPuntaje;
 }
 FechaLanzamiento crearFecha(int dia, int mes, int anio)
 {
-	printf("todavia no se implemento la funcion\n");
-	FechaLanzamiento j = { 0 };
-	return j;
+	FechaLanzamiento nuevaFecha;
+
+	nuevaFecha.esValido = 1;
+
+	if (dia < 1 || dia > 31)
+	{
+		nuevaFecha.esValido = 0;
+
+	}
+	else
+	{
+		nuevaFecha.dia = dia;
+	}
+
+	if (mes < 1 || mes > 12)
+	{
+		nuevaFecha.esValido =0
+	}
+	else
+	{
+		nuevaFecha.mes = mes;
+	}
+
+	if(anio <= 1950 || anio >= 2030)
+	{
+		nuevaFecha.esValido = 0;
+	}
+	else
+	{
+		nuevaFecha.anio = anio;
+	}
+
+	return nuevaFecha;
 }
