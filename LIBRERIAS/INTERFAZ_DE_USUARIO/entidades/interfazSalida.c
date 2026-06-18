970e6db068b691e274d54fac327d5e32cf9f6002
Kevin Perez <kevinnicolas92@gmail.com>
2026-06-17 21:59:22 -0300

    se implemento funcion "mostrarListadoCategorias"

--- LIBRERIAS/INTERFAZ_DE_USUARIO/entidades/interfazSalida.c
+++ LIBRERIAS/INTERFAZ_DE_USUARIO/entidades/interfazSalida.c
@@ -5,6 +5,8 @@
 #include "../cabeceraEntidades/interfazSalida.h"
 #include "../../DOMINIO/cabeceraEntidades/juegos.h"
 #include "../../SERVICIO/cabeceraEntidades/logicaJuego.h"
+#include "../../DOMINIO/cabeceraEntidades/categoria.h"
+#include "../../SERVICIO/cabeceraEntidades/logicaCategoria.h"
 
 void mostrarListadoJuegos()
 {
@@ -29,8 +31,29 @@ void mostrarListadoJuegos()
 }
 void mostrarListadoCategorias()
 {
-	
+	int validos = 0;
+
+	Categoria* lista = obtenerListadoCategoriasDinamico(&validos);
+
+	if(lista == NULL)
+	{
+		printf("\nNo hay categorias para mostrar.\n");
+		return;
+	}
+
+	ordenarCategoriasAlfabeticamente(lista, validos);
+
+	printf("========== LISTADO DE CATEGORIAS ==========\n");
+	for(int i = 0; i < validos; i++)
+	{ 
+		printf("ID: %d\n", lista[i].idCategoria);
+		printf("Nombre: %s\n", lista[i].nombre);
+		printf("----------------------------------\n");
+	}
+
+	free(lista);
 }
+
 void mostrarRankingNominacionesUI()
 {
 	printf("Todavia no se implemento esta funcion.\n");
