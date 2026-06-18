4845afbb106abfcd2f90bf9ad50175c6edf5503d
jzoemartina-debug <jzoemartina@gmail.com>
2026-06-18 03:12:55 -0300

    .

--- LIBRERIAS/SERVICIO/entidades/logicaNominaciones.c
+++ LIBRERIAS/SERVICIO/entidades/logicaNominaciones.c
@@ -10,15 +10,19 @@ int registrarNominacion(int puntajeValor, int d, int m, int a)
 {
 	FILE *fp = fopen(ARCHIVO_NOMINACIONES, "rb");
 
-	if(existeNominacionDuplicada(idJuego, idCategoria))
+	Nominacion nominacion;
+
+	if(fp != NULL)
     {
-		return -2;
+		while(fread(&nominacion, sizeof(Nominacion), 1, fp) > 0)
+		{
+			if(existeNominacionDuplicada(idJuego))
+			{
+				return -2;
+			}
+		}
 	}
-	{
-		fclose(fp);
-		return 0;
-	}
-	printf("funcion no implementada\n");
+	fclose(fp);
 	return 0;
 }
 int existeNominacionDuplicada(int idJuego, int idCategoria)
@@ -26,25 +30,60 @@ int existeNominacionDuplicada(int idJuego, int idCategoria)
 	FILE* fp = fopen(ARCHIVO_NOMINACIONES, "rb");
 
 	Nominacion nominacion;
+	int nominacionEncontrada = 0;
 
 	if(fp != NULL)
 	{
-		while (fread());
+		while (fread(&nominacion, sizeof(Nominacion), 1, fp) > 0)
+		{
+			if(idJuego == idJuego && nominacion.idCategoria == idCategoria)
+			{
+				nominacionEncontrada = 1;
+			}
+		}
 	}
-
-
-	printf("funcion no implementada\n");
-	return 0;
+	fclose(fp);
+	return nominacionEncontrada;
 }
 Pila obtenerRankingNominaciones()
 {
-	printf("funcion no implementada\n");
+	FILE* fp = fopen(ARCHIVO_NOMINACIONES, "rb");
+
+	Nominacion nominacion;
 	Pila p;
 	inicpila(&p);
+	int mayorPuntaje = 0;
+
+	if (fp != NULL)
+	{
+		while (fread(&nominacion, sizeof(Nominacion), 1, fp) > 0)
+		{
+			if (nominacion.puntaje.valor > mayorPuntaje)
+			{
+				mayorPuntaje = nominacion.puntaje.valor;
+				apilar(&p, mayorPuntaje);
+			}
+		}
+	}
+	fclose(fp);
 	return p;
 }
 void exportarNominacionesATexto(char rutaTexto[])
 {
-	printf("funcion no implementada\n");
-}
+	FILE *fp = fopen(ARCHIVO_NOMINACIONES, "rb");
 
+	FILE* fTxt = fopen("nominaciones.txt", "w");
+
+	Nominacion nominacion;
+
+	if (fp != NULL && fTxt != NULL)
+	{
+		fprintf(fTxt, "ID Nominacion | Puntaje | Fecha Lanzamiento\n");
+	}
+	while(fread(&nominacion, sizeof(Nominacion), 1 , fp)> 0)
+	{
+		fprintf(fTxt, "%d | %d | %02d/%02d/%04d\n", nominacion.idNominacion, nominacion.puntaje.valor, nominacion.fecha.dia, nominacion.fecha.mes, nominacion.fecha.anio);
+	}
+	fclose(fp);
+	fclose(fTxt);
+}
