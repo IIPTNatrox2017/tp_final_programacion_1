8e100b0073f7f576f4b60b041f729bd058b53766
jzoemartina-debug <jzoemartina@gmail.com>
2026-06-18 02:03:06 -0300

    .

--- LIBRERIAS/SERVICIO/entidades/logicaNominaciones.c
+++ LIBRERIAS/SERVICIO/entidades/logicaNominaciones.c
@@ -3,15 +3,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define ARCHIVO_NOMINACIONES "nominaciones.bin"
 
 int registrarNominacion(int idJuego, int idCategoria, float puntajeValor, int d, int m, int a)
 {
-	int a;
+	FILE *fp = fopen(ARCHIVO_NOMINACIONES, "rb");
+
+	if (existeNominacionDuplicada(idJuego, idCategoria))
+    {
+		return -2;
+	}
+	{
+		fclose(fp);
+		return 0;
+	}
 	printf("funcion no implementada\n");
 	return 0;
 }
 int existeNominacionDuplicada(int idJuego, int idCategoria)
 {
+	FILE* fp = fopen(ARCHIVO_NOMINACIONES, "rb");
+
+	Nominacion nominacion;
+
+	if(fp != NULL)
+	{
+		while (fread());
+	}
+
+
 	printf("funcion no implementada\n");
 	return 0;
 }
