4389f2bddbe46c2d03d04e868cf328beba52d64d
Kevin Perez <kevinnicolas92@gmail.com>
2026-06-20 23:48:56 -0300

    se simplifico el menus.c

--- LIBRERIAS/INTERFAZ_DE_USUARIO/entidades/menus.c
+++ LIBRERIAS/INTERFAZ_DE_USUARIO/entidades/menus.c
@@ -104,7 +104,7 @@ void subMenuJuegos()
 	do
 	{
 		mostrarSubMenuJuegos();
-		opcion = pedirOpcionSubMenuJuegos();
+		opcion = pedirOpcion();
 		ejecutarOpcionSubMenuJuegos(opcion);
 	} while (opcion != 0);
 }
@@ -121,16 +121,6 @@ void mostrarSubMenuJuegos()
 	printf("3. Ordenar juegos por ID.\n");
 	printf("4. Vaciar Archivo juegos.\n");
 	printf("0. Volver al Menu Principal\n");
-}
-
-int pedirOpcionSubMenuJuegos()
-{
-	int opcion;
-
-	printf("Ingrese una opcion\n>>> ");
-	opcion = scanInt();
-
-	return opcion;
 }
 
 void ejecutarOpcionSubMenuJuegos(int opcion)
@@ -188,7 +178,7 @@ void subMenuCategorias()
 	do
 	{
 		mostrarSubMenuCategorias();
-		opcion = pedirOpcionSubMenuCategorias();
+		opcion = pedirOpcion();
 		ejecutarOpcionSubMenuCategorias(opcion);
 	} while (opcion != 0);
 }
@@ -204,16 +194,6 @@ void mostrarSubMenuCategorias()
 	printf("2. Listado Categorias.\n");
 	printf("3. Abrir archivo limpio.\n");
 	printf("0. Volver al Menu Principal\n");
-}
-
-int pedirOpcionSubMenuCategorias()
-{
-	int opcion;
-
-	printf("Ingrese una opcion\n>>> ");
-	opcion = scanInt();
-
-	return opcion;
 }
 
 void ejecutarOpcionSubMenuCategorias(int opcion)
@@ -268,7 +248,7 @@ void subMenuNominaciones()
 	do
 	{
 		mostrarSubMenuNominaciones();
-		opcion = pedirOpcionSubMenuNominaciones();
+		opcion = pedirOpcion();
 		ejecutarOpcionSubMenuNominaciones(opcion);
 	} while (opcion != 0);
 }
@@ -283,16 +263,6 @@ void mostrarSubMenuNominaciones()
 	printf("1. Registrar Nominacion.\n");
 	printf("2. Ranking Nominaciones.\n");
 	printf("0. Volver al Menu Principal\n");
-}
-
-int pedirOpcionSubMenuNominaciones()
-{
-	int opcion;
-
-	printf("Ingrese una opcion\n >>> ");
-	opcion = scanInt();
-
-	return opcion;
 }
 
 void ejecutarOpcionSubMenuNominaciones(int opcion)
