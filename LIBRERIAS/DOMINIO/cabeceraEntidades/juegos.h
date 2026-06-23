990027278f7972e4018f6f7d84655d149d1f028b
jzoemartina-debug <jzoemartina@gmail.com>
2026-06-23 05:02:56 -0300

    comentarios de Doxygen en funciones de categorias

--- LIBRERIAS/DOMINIO/cabeceraEntidades/juegos.h
+++ LIBRERIAS/DOMINIO/cabeceraEntidades/juegos.h
@@ -12,8 +12,17 @@ typedef struct stJuego
 
 } Juego;
 
-
+/**
+* @brief Crea un nuevo juego con valores iniciales.
+* 
+* @param id ID del juego.
+* @param nombre Nombre del juego.
+* @param estudio Estudio desarrollador del juego.
+* @param categoria Nombre de la categoria del juego.
+* @return Juego Juego creado e inicializado.
+*/
 Juego crearJuego(int id, char nombre[], char estudio[], char categoria[]);
+
 
 int compararJuegosPorNombre(Juego j1, Juego j2);
 int compararJuegosPorId(Juego j1, Juego j2);
